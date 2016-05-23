e1c5f38a111e2ff0b7610fba40e81a8912d3b7f0
Carpago Software | Raymond Loman <raymond@carpago.nl>
2016-05-23 17:04:31 +0200

    Inject repository into service and add save method

--- src/main/java/nl/programit/people/persistence/PersonService.java
+++ src/main/java/nl/programit/people/persistence/PersonService.java
@@ -1,10 +1,23 @@
 package nl.programit.people.persistence;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import nl.programit.people.domain.Person;
 
 @Service
 @Transactional
 public class PersonService {
+	
+	@Autowired
+	private PersonRepository personRepository;
+	
+	public void save(Person person) {
+		
+		
+		this.personRepository.save(person);		
+
+	}
 
 }
