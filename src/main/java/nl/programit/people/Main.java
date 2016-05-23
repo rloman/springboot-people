464d72ea18932a1dc7fa647d3478ac8c00f5172f
Carpago Software | Raymond Loman <raymond@carpago.nl>
2016-05-23 16:58:47 +0200

    Add some setup

--- src/main/java/nl/programit/people/Main.java
+++ src/main/java/nl/programit/people/Main.java
@@ -2,7 +2,6 @@ package nl.programit.people;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class Main {
